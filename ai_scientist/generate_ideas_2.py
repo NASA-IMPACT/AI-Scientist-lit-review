cfce8f6de7a926052c949658ae0edf7088f6f0a1
NISH1001 <nishanpantha@gmail.com>
2024-10-09 16:34:09 -0500

    Add `generate_ideas_2` to use paper-qa2 to generate summary

---
+++ ai_scientist/generate_ideas_2.py
@@ -0,0 +1,257 @@
+import glob
+import json
+import os
+import os.path as osp
+import pickle
+import time
+from collections import defaultdict
+from datetime import datetime
+from typing import Dict, List, Optional, Union
+
+import backoff
+import numpy as np
+import requests
+from loguru import logger
+from paperqa import Answer, Context, Doc, Docs, Settings, Text, ask
+from tqdm import tqdm
+
+from ai_scientist.llm import extract_json_between_markers, get_response_from_llm
+
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            # Convert datetime to string in ISO format
+            return obj.isoformat()
+        elif isinstance(obj, set):
+            # Convert set to list
+            return list(obj)
+        return super().default(obj)
+
+
+def group_contexts_by_doc(contexts: List[dict]) -> Dict[str, Dict]:
+    grouped_contexts = defaultdict(lambda: {"doc_details": None, "contexts": []})
+
+    for context in contexts:
+        doc_id = context["doc"]["doc_id"]
+        doc_key = context["doc"]["key"]
+
+        # If doc_details are not set for this doc_id, assign them
+        if grouped_contexts[doc_key]["doc_details"] is None:
+            grouped_contexts[doc_key]["doc_details"] = context["doc"]
+
+        # Add the context to the list of contexts for this document
+        grouped_contexts[doc_key]["contexts"].append(
+            {
+                "context": context["context"],
+                "score": context["score"],
+                "text": context["text"],
+            },
+        )
+
+    return dict(grouped_contexts)
+
+
+def _flatten_pqa_answer(answer: Answer) -> dict:
+    res = answer.model_dump()
+    contexts = []
+    for context in answer.contexts:
+        dct = dict(
+            context=context.context,
+            score=context.score,
+            text=context.text.text,
+            doc=context.text.doc.model_dump(),
+        )
+        contexts.append(dct)
+    res["contexts"] = contexts
+    res.pop("context", None)
+    res.pop("id", None)
+    return res
+
+
+def generate_summary(
+    ideas,
+    base_dir,
+    client,
+    model,
+    paper_qa_obj,
+    debug: bool = False,
+) -> List[dict]:
+    res = []
+    total_cost = 0
+    token_counts = 0
+
+    # if ideas not passed, try loading
+    if not ideas:
+        logger.debug(f"Loading ideas from ideas.json")
+        with open(osp.join(base_dir, "ideas.json")) as f:
+            ideas = json.load(f)
+
+    unwanted_keys = ["formatted_answer", "config_md5"]
+    for idx, idea in tqdm(enumerate(ideas)):
+        answer = _flatten_pqa_answer(paper_qa_obj.query(idea))
+        answer["documents"] = group_contexts_by_doc(answer.pop("contexts", []))
+        for _uk in unwanted_keys:
+            answer.pop(_uk, None)
+        logger.info(f"Idea = {idea} | answer={answer['answer']}")
+        res.append(answer)
+        total_cost += answer["cost"]
+        token_counts += np.sum(list(answer["token_counts"].values()))
+        if debug:
+            logger.debug(
+                f"Currently cost at {round(total_cost, 3)} and {token_counts} tokens.",
+            )
+
+    summary_path = osp.join(base_dir, "summary.json")
+    logger.info(f"Dumping summary to {summary_path}")
+    with open(summary_path, "w") as f:
+        json.dump(res, f, indent=4, cls=CustomJSONEncoder)
+    logger.info(
+        f"Total cost :: {round(total_cost, 3)} | Token Counts :: {token_counts}",
+    )
+    return res
+
+
+def build_paperqa_index(
+    paths: List[str],
+    index_path: Optional[os.PathLike] = None,
+) -> Docs:
+    docs = Docs()
+
+    # Check if the index path is provided and if the index file exists
+    if index_path and osp.isdir(index_path):
+        index_path = osp.join(index_path, "pqa.pkl")
+    if index_path and osp.isfile(index_path):
+        logger.info(f"Loading from saved index :: {index_path}")
+        with open(index_path, "rb") as f:
+            docs = pickle.load(f)
+        return docs
+
+    # If index_path does not exist or file doesn't exist, build a new index
+    logger.info(f"Building new index for {len(paths)} files")
+    for path in paths:
+        if path.startswith(("http", "www.")):
+            docs.add_url(path)
+        else:
+            docs.add(path)
+
+    # Save the new index if index_path is provided
+    if index_path:
+        # Default to pqa.pkl if index_path is a directory
+        if osp.isdir(index_path):
+            index_path = osp.join(index_path, "pqa.pkl")
+
+        # Log saving operation (assuming logger is defined elsewhere)
+        logger.info(f"Saving to {index_path}")
+
+        # Save the docs object to the index_path file
+        with open(index_path, "wb") as f:
+            pickle.dump(docs, f)
+
+    return docs
+
+
+if __name__ == "__main__":
+    MAX_NUM_GENERATIONS = 32
+    NUM_REFLECTIONS = 5
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Generate AI scientist ideas")
+    # add type of experiment (nanoGPT, Boston, etc.)
+    parser.add_argument(
+        "--experiment",
+        type=str,
+        default="nanoGPT",
+        help="Experiment to run AI Scientist on.",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="gpt-4o-2024-05-13",
+        choices=[
+            "claude-3-5-sonnet-20240620",
+            "gpt-4o-2024-05-13",
+            "deepseek-coder-v2-0724",
+            "llama3.1-405b",
+        ],
+        help="Model to use for AI Scientist.",
+    )
+    parser.add_argument(
+        "--skip-idea-generation",
+        action="store_true",
+        help="Skip idea generation and use existing ideas.",
+    )
+    parser.add_argument(
+        "--check-novelty",
+        action="store_true",
+        help="Check novelty of ideas.",
+    )
+    args = parser.parse_args()
+
+    # Create client
+    if args.model == "claude-3-5-sonnet-20240620":
+        import anthropic
+
+        print(f"Using Anthropic API with model {args.model}.")
+        client_model = "claude-3-5-sonnet-20240620"
+        client = anthropic.Anthropic()
+    elif args.model.startswith("bedrock") and "claude" in args.model:
+        import anthropic
+
+        # Expects: bedrock/<MODEL_ID>
+        client_model = args.model.split("/")[-1]
+
+        print(f"Using Amazon Bedrock with model {client_model}.")
+        client = anthropic.AnthropicBedrock()
+    elif args.model.startswith("vertex_ai") and "claude" in args.model:
+        import anthropic
+
+        # Expects: vertex_ai/<MODEL_ID>
+        client_model = args.model.split("/")[-1]
+
+        print(f"Using Vertex AI with model {client_model}.")
+        client = anthropic.AnthropicVertex()
+    elif args.model == "gpt-4o-2024-05-13":
+        import openai
+
+        print(f"Using OpenAI API with model {args.model}.")
+        client_model = "gpt-4o-2024-05-13"
+        client = openai.OpenAI()
+    elif args.model == "deepseek-coder-v2-0724":
+        import openai
+
+        print(f"Using OpenAI API with {args.model}.")
+        client_model = "deepseek-coder-v2-0724"
+        client = openai.OpenAI(
+            api_key=os.environ["DEEPSEEK_API_KEY"],
+            base_url="https://api.deepseek.com",
+        )
+    elif args.model == "llama3.1-405b":
+        import openai
+
+        print(f"Using OpenAI API with {args.model}.")
+        client_model = "meta-llama/llama-3.1-405b-instruct"
+        client = openai.OpenAI(
+            api_key=os.environ["OPENROUTER_API_KEY"],
+            base_url="https://openrouter.ai/api/v1",
+        )
+    else:
+        raise ValueError(f"Model {args.model} not supported.")
+
+    base_dir = osp.join("templates", args.experiment)
+    results_dir = osp.join("results", args.experiment)
+    paper_paths = glob.glob(osp.join(base_dir, "papers", "*.pdf"))
+
+    questions = []
+    with open(osp.join(base_dir, "ideas.json")) as f:
+        questions = json.load(f)
+
+    pqa_docs = build_paperqa_index(paper_paths, index_path=base_dir)
+    summary = generate_summary(
+        ideas=questions,
+        base_dir=base_dir,
+        client=client,
+        model=client_model,
+        paper_qa_obj=pqa_docs,
+        debug=True,
+    )
---
+++ launch_scientist_lit.py
@@ -0,0 +1,392 @@
+import argparse
+import glob
+import json
+import multiprocessing
+import os
+import os.path as osp
+import pprint
+import shutil
+import sys
+import time
+from datetime import datetime
+
+import openai
+import torch
+from aider.coders import Coder
+from aider.io import InputOutput
+from aider.models import Model
+
+from ai_scientist.generate_ideas import check_idea_novelty, generate_ideas
+from ai_scientist.generate_ideas_2 import build_paperqa_index, generate_summary
+from ai_scientist.perform_experiments import perform_experiments
+from ai_scientist.perform_review import load_paper, perform_improvement, perform_review
+from ai_scientist.perform_writeup import generate_latex, perform_writeup
+
+NUM_REFLECTIONS = 3
+
+
+def print_time():
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Run AI scientist experiments")
+    parser.add_argument(
+        "--skip-idea-generation",
+        action="store_true",
+        help="Skip idea generation and load existing ideas",
+    )
+    parser.add_argument(
+        "--skip-novelty-check",
+        action="store_true",
+        help="Skip novelty check and use existing ideas",
+    )
+    # add type of experiment (nanoGPT, Boston, etc.)
+    parser.add_argument(
+        "--experiment",
+        type=str,
+        default="nanoGPT",
+        help="Experiment to run AI Scientist on.",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="claude-3-5-sonnet-20240620",
+        choices=[
+            "claude-3-5-sonnet-20240620",
+            "gpt-4o-2024-05-13",
+            "deepseek-coder-v2-0724",
+            "llama3.1-405b",
+            # Anthropic Claude models via Amazon Bedrock
+            "bedrock/anthropic.claude-3-sonnet-20240229-v1:0",
+            "bedrock/anthropic.claude-3-5-sonnet-20240620-v1:0",
+            "bedrock/anthropic.claude-3-haiku-20240307-v1:0",
+            "bedrock/anthropic.claude-3-opus-20240229-v1:0"
+            # Anthropic Claude models Vertex AI
+            "vertex_ai/claude-3-opus@20240229",
+            "vertex_ai/claude-3-5-sonnet@20240620",
+            "vertex_ai/claude-3-sonnet@20240229",
+            "vertex_ai/claude-3-haiku@20240307",
+        ],
+        help="Model to use for AI Scientist.",
+    )
+    parser.add_argument(
+        "--writeup",
+        type=str,
+        default="latex",
+        choices=["latex"],
+        help="What format to use for writeup",
+    )
+    parser.add_argument(
+        "--parallel",
+        type=int,
+        default=0,
+        help="Number of parallel processes to run. 0 for sequential execution.",
+    )
+    parser.add_argument(
+        "--improvement",
+        action="store_true",
+        help="Improve based on reviews.",
+    )
+    parser.add_argument(
+        "--gpus",
+        type=str,
+        default=None,
+        help="Comma-separated list of GPU IDs to use (e.g., '0,1,2'). If not specified, all available GPUs will be used.",
+    )
+    parser.add_argument(
+        "--num-ideas",
+        type=int,
+        default=50,
+        help="Number of ideas to generate",
+    )
+    return parser.parse_args()
+
+
+def get_available_gpus(gpu_ids=None):
+    if gpu_ids is not None:
+        return [int(gpu_id) for gpu_id in gpu_ids.split(",")]
+    return list(range(torch.cuda.device_count()))
+
+
+def worker(
+    queue,
+    base_dir,
+    results_dir,
+    model,
+    client,
+    client_model,
+    writeup,
+    improvement,
+    gpu_id,
+):
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+    print(f"Worker {gpu_id} started.")
+    while True:
+        idea = queue.get()
+        if idea is None:
+            break
+        success = do_idea(
+            base_dir,
+            results_dir,
+            idea,
+            model,
+            client,
+            client_model,
+            writeup,
+            improvement,
+            log_file=True,
+        )
+        print(f"Completed idea: {idea['Name']}, Success: {success}")
+    print(f"Worker {gpu_id} finished.")
+
+
+def do_idea(
+    base_dir,
+    results_dir,
+    idea,
+    model,
+    client,
+    client_model,
+    writeup,
+    improvement,
+    log_file=False,
+):
+    ## CREATE PROJECT FOLDER
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    idea_name = f"{timestamp}_{idea['Name']}"
+    folder_name = osp.join(results_dir, idea_name)
+    assert not osp.exists(folder_name), f"Folder {folder_name} already exists."
+    destination_dir = folder_name
+    shutil.copytree(base_dir, destination_dir, dirs_exist_ok=True)
+    with open(osp.join(base_dir, "run_0", "final_info.json"), "r") as f:
+        baseline_results = json.load(f)
+    baseline_results = {k: v["means"] for k, v in baseline_results.items()}
+    exp_file = osp.join(folder_name, "experiment.py")
+    vis_file = osp.join(folder_name, "plot.py")
+    notes = osp.join(folder_name, "notes.txt")
+    with open(notes, "w") as f:
+        f.write(f"# Title: {idea['Title']}\n")
+        f.write(f"# Experiment description: {idea['Experiment']}\n")
+        f.write(f"## Run 0: Baseline\n")
+        f.write(f"Results: {baseline_results}\n")
+        f.write(f"Description: Baseline results.\n")
+    if log_file:
+        original_stdout = sys.stdout
+        original_stderr = sys.stderr
+        log_path = osp.join(folder_name, "log.txt")
+        log = open(log_path, "a")
+        sys.stdout = log
+        sys.stderr = log
+    try:
+        print_time()
+        print(f"*Starting idea: {idea_name}*")
+        ## PERFORM EXPERIMENTS
+        fnames = [exp_file, vis_file, notes]
+        io = InputOutput(
+            yes=True,
+            chat_history_file=f"{folder_name}/{idea_name}_aider.txt",
+        )
+        if model == "deepseek-coder-v2-0724":
+            main_model = Model("deepseek/deepseek-coder")
+        elif model == "llama3.1-405b":
+            main_model = Model("openrouter/meta-llama/llama-3.1-405b-instruct")
+        else:
+            main_model = Model(model)
+        coder = Coder.create(
+            main_model=main_model,
+            fnames=fnames,
+            io=io,
+            stream=False,
+            use_git=False,
+            edit_format="diff",
+        )
+
+        print_time()
+        print(f"*Starting Experiments*")
+        try:
+            success = perform_experiments(idea, folder_name, coder, baseline_results)
+        except Exception as e:
+            print(f"Error during experiments: {e}")
+            print(f"Experiments failed for idea {idea_name}")
+            return False
+
+        if not success:
+            print(f"Experiments failed for idea {idea_name}")
+            return False
+
+        print_time()
+        print(f"*Starting Writeup*")
+        ## PERFORM WRITEUP
+        if writeup == "latex":
+            writeup_file = osp.join(folder_name, "latex", "template.tex")
+            fnames = [exp_file, writeup_file, notes]
+            if model == "deepseek-coder-v2-0724":
+                main_model = Model("deepseek/deepseek-coder")
+            elif model == "llama3.1-405b":
+                main_model = Model("openrouter/meta-llama/llama-3.1-405b-instruct")
+            else:
+                main_model = Model(model)
+            coder = Coder.create(
+                main_model=main_model,
+                fnames=fnames,
+                io=io,
+                stream=False,
+                use_git=False,
+                edit_format="diff",
+            )
+            try:
+                perform_writeup(idea, folder_name, coder, client, client_model)
+            except Exception as e:
+                print(f"Failed to perform writeup: {e}")
+                return False
+            print("Done writeup")
+        else:
+            raise ValueError(f"Writeup format {writeup} not supported.")
+
+        print_time()
+        print(f"*Starting Review*")
+        ## REVIEW PAPER
+        if writeup == "latex":
+            try:
+                paper_text = load_paper(f"{folder_name}/{idea['Name']}.pdf")
+                review = perform_review(
+                    paper_text,
+                    model="gpt-4o-2024-05-13",
+                    client=openai.OpenAI(),
+                    num_reflections=5,
+                    num_fs_examples=1,
+                    num_reviews_ensemble=5,
+                    temperature=0.1,
+                )
+                # Store the review in separate review.txt file
+                with open(osp.join(folder_name, "review.txt"), "w") as f:
+                    f.write(json.dumps(review, indent=4))
+            except Exception as e:
+                print(f"Failed to perform review: {e}")
+                return False
+
+        ## IMPROVE WRITEUP
+        if writeup == "latex" and improvement:
+            print_time()
+            print(f"*Starting Improvement*")
+            try:
+                perform_improvement(review, coder)
+                generate_latex(
+                    coder,
+                    folder_name,
+                    f"{folder_name}/{idea['Name']}_improved.pdf",
+                )
+                paper_text = load_paper(f"{folder_name}/{idea['Name']}_improved.pdf")
+                review = perform_review(
+                    paper_text,
+                    model="gpt-4o-2024-05-13",
+                    client=openai.OpenAI(),
+                    num_reflections=5,
+                    num_fs_examples=1,
+                    num_reviews_ensemble=5,
+                    temperature=0.1,
+                )
+                # Store the review in separate review.txt file
+                with open(osp.join(folder_name, "review_improved.txt"), "w") as f:
+                    f.write(json.dumps(review))
+            except Exception as e:
+                print(f"Failed to perform improvement: {e}")
+                return False
+        return True
+    except Exception as e:
+        print(f"Failed to evaluate idea {idea_name}: {str(e)}")
+        return False
+    finally:
+        print("FINISHED IDEA")
+        if log_file:
+            sys.stdout = original_stdout
+            sys.stderr = original_stderr
+            log.close()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    # Check available GPUs and adjust parallel processes if necessary
+    available_gpus = get_available_gpus(args.gpus)
+    if args.parallel > len(available_gpus):
+        print(
+            f"Warning: Requested {args.parallel} parallel processes, but only {len(available_gpus)} GPUs available. Adjusting to {len(available_gpus)}.",
+        )
+        args.parallel = len(available_gpus)
+
+    print(f"Using GPUs: {available_gpus}")
+
+    # Create client
+    if args.model == "claude-3-5-sonnet-20240620":
+        import anthropic
+
+        print(f"Using Anthropic API with model {args.model}.")
+        client_model = "claude-3-5-sonnet-20240620"
+        client = anthropic.Anthropic()
+    elif args.model.startswith("bedrock") and "claude" in args.model:
+        import anthropic
+
+        # Expects: bedrock/<MODEL_ID>
+        client_model = args.model.split("/")[-1]
+
+        print(f"Using Amazon Bedrock with model {client_model}.")
+        client = anthropic.AnthropicBedrock(
+            aws_access_key=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            aws_region=os.getenv("AWS_REGION_NAME"),
+        )
+    elif args.model.startswith("vertex_ai") and "claude" in args.model:
+        import anthropic
+
+        # Expects: vertex_ai/<MODEL_ID>
+        client_model = args.model.split("/")[-1]
+
+        print(f"Using Vertex AI with model {client_model}.")
+        client = anthropic.AnthropicVertex()
+    elif args.model == "gpt-4o-2024-05-13":
+        import openai
+
+        print(f"Using OpenAI API with model {args.model}.")
+        client_model = "gpt-4o-2024-05-13"
+        client = openai.OpenAI()
+    elif args.model == "deepseek-coder-v2-0724":
+        import openai
+
+        print(f"Using OpenAI API with {args.model}.")
+        client_model = "deepseek-coder-v2-0724"
+        client = openai.OpenAI(
+            api_key=os.environ["DEEPSEEK_API_KEY"],
+            base_url="https://api.deepseek.com",
+        )
+    elif args.model == "llama3.1-405b":
+        import openai
+
+        print(f"Using OpenAI API with {args.model}.")
+        client_model = "meta-llama/llama-3.1-405b-instruct"
+        client = openai.OpenAI(
+            api_key=os.environ["OPENROUTER_API_KEY"],
+            base_url="https://openrouter.ai/api/v1",
+        )
+    else:
+        raise ValueError(f"Model {args.model} not supported.")
+
+    base_dir = osp.join("templates", args.experiment)
+    results_dir = osp.join("results", args.experiment)
+    paper_paths = glob.glob(osp.join(base_dir, "papers", "*.pdf"))
+
+    ideas = []
+    with open(osp.join(base_dir, "ideas.json")) as f:
+        questions = json.load(f)
+
+    pqa_docs = build_paperqa_index(paper_paths, index_path=base_dir)
+    summary = generate_summary(
+        ideas=questions,
+        base_dir=base_dir,
+        client=client,
+        model=client_model,
+        paper_qa_obj=pqa_docs,
+        debug=True,
+    )
+    # pprint.pprint(summary)
